d6f0dc5343652aff1757d3ef2bf82fd72eed3806
yuppne <cyubin105@naver.com>
2023-01-11 17:40:55 +0900

    fix multiple definition error 2

--- src/t_hash.c
+++ src/t_hash.c
@@ -32,7 +32,7 @@
 
 #define PER_MICROSEC 1000
 const long long NANOS = 1000000000LL;
-struct timespec tstart={0,0}, tend={0,0},tstart2={0,0}, tend2={0,0};
+static struct timespec tstart={0,0}, tend={0,0},tstart2={0,0}, tend2={0,0};
 static long long duration = 0;
 static long long duration2 = 0;
 
